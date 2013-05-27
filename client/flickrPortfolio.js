c5063d707ced450463c81975422ba76dd7aef00b
shongololo <garethsimons@icloud.com>
2013-05-26 23:28:07 -0500

    collection restructuring, generally working, but subscribe and publish still being set up.

--- client/flickrPortfolio.js
+++ client/flickrPortfolio.js
@@ -6,33 +6,39 @@
 flickrDB = new Meteor.Collection("flickrDB");
 
 //// Variables:
+var flickrDBKey = "flickrSets";
 // Currently selected set for photo display.
 Session.setDefault('setID', null);
 
 Meteor.startup(function(){
 	Meteor.subscribe("sets");
-	//Deps.autorun(function(){
-	//	Meteor.subscribe("sets",Session.get("flickrSetID"));
-	//});
+	Deps.autorun(function(){
+		var flickrSetID = Session.get("setID");
+		Meteor.subscribe("photos",flickrSetID);
+	});
 });
 
-Template.backgroundImage.background = function(){
+//Template.backgroundImage.background = function(){
 	//FlickrRandomPhotoFromSet(apiKey,setID);
 	//return Session.get("RandomURL");
-};
+//};
 
 Template.setsBrowser.sets = function(){
-	return flickrDB.find({name:"flickrSet"});
+	var test = flickrDB.find({name:flickrDBKey});
+	console.log(test);//doesn't work?
+	return flickrDB.find({name:flickrDBKey});
 };
 
 Template.setsBrowser.events = ({
 	'click img' : function (event,template) {
-		Session.set("flickrSetID",this.data.id);
+		Session.set("setID",this.data.id);
 	}
 });
 
 Template.photoBrowser.photos = function () {
-	//return photoReturn(Session.get(""));
+	var test = flickrDB.find({name:"photo"});
+	console.log(test);// doesn't work?
+	return flickrDB.find({name:"photo"});
 };
 
 Template.photoBrowser.events = ({
