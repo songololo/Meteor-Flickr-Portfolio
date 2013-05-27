e42172e2724e87c01b129a45f1c51262ca527f39
shongololo <garethsimons@icloud.com>
2013-05-26 23:28:05 -0500

    collection restructuring, generally working, but subscribe and publish still being set up.

--- server/flickrPortfolioPublish.js
+++ server/flickrPortfolioPublish.js
@@ -10,15 +10,29 @@ flickrDB = new Meteor.Collection("flickrDB");
 var apiKey = "b4b033a1b3c8f74573e021bd37565336";
 var userName = "garethsimons";
 var userID = "78352164@N07";
+var flickrDBKey = "flickrSets";
 
 Meteor.startup(function () {	
-	FlickrSetList(apiKey,userID,flickrDB,"flickrSet",function(){
+	FlickrSetList(apiKey,userID,flickrDB,flickrDBKey,function(){
+		var setList = flickrDB.find({name:flickrDBKey});
+		setList.forEach(function(eachSetItem){
+			var flickrSetID=eachSetItem.data.id;
+			FlickrSetPhotos(apiKey,flickrSetID,flickrDBKey);
+		});
 		Meteor.publish("sets",function(){
-			return flickrDB.find();
+			return flickrDB.find(
+				{},//find all documents
+				{id:1,data:1}//return only selected - doesn't work?
+			);
+		});
+		Meteor.publish("photos",function(setID){
+			return flickrDB.find(
+				{"id":setID},
+				{"photos.photo":1}
+				);
 		});
 	});
 });
-
 //// Publish:
 // Publish sets collection to client.
 
