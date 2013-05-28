1710ddd68e3df7b035f48e523c655029ac788471
shongololo <garethsimons@icloud.com>
2013-05-27 22:29:31 -0500

    Restructured. Pending HTML and CSS refinement.

--- server/flickrPortfolioPublish.js
+++ server/flickrPortfolioPublish.js
@@ -1,9 +1,13 @@
 // Created by Gareth Simons.
 // Initial inspiration from flixploretest.
 
+
+
 //// Collections:
 // Create flickr server-side collection.
 flickrDB = new Meteor.Collection("flickrDB");
+
+
 
 //// Variables:
 // Flickr variables:
@@ -11,6 +15,8 @@ var apiKey = "b4b033a1b3c8f74573e021bd37565336";
 var userName = "garethsimons";
 var userID = "78352164@N07";
 var flickrDBKey = "flickrSets";
+
+
 
 Meteor.startup(function () {	
 	FlickrSetList(apiKey,userID,flickrDB,flickrDBKey,function(){
@@ -21,23 +27,9 @@ Meteor.startup(function () {
 		});
 		Meteor.publish("sets",function(){
 			return flickrDB.find(
-				{},//find all documents
-				{id:1,data:1}//return only selected - doesn't work?
+				{},
+				{fields: {id:1,data:1,photos:1}}
 			);
-		});
-		Meteor.publish("photos",function(setID){
-			return flickrDB.find(
-				{"id":setID},
-				{"photos.photo":1}
-				);
 		});
 	});
 });
-//// Publish:
-// Publish sets collection to client.
-
-
-// Publish photos to client.
-//Meteor.publish("photos",function(){
-//	return flickrDB.find();
-//});
