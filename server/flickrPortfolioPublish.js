2814a5c553e3841b96e6dc782aea07871c711537
shongololo <garethsimons@icloud.com>
2013-05-28 23:12:50 -0500

    Ongoing refinements. Basic HTML structure.

--- server/flickrPortfolioPublish.js
+++ server/flickrPortfolioPublish.js
@@ -3,33 +3,29 @@
 
 
 
-//// Collections:
-// Create flickr server-side collection.
-flickrDB = new Meteor.Collection("flickrDB");
-
-
-
-//// Variables:
-// Flickr variables:
+//// VARIABLES:
 var apiKey = "b4b033a1b3c8f74573e021bd37565336";
 var userName = "garethsimons";
-var userID = "78352164@N07";
+userID = null //"78352164@N07"; Global variable - set in flickr.js
 var flickrDBKey = "flickrSets";
 
 
 
+//// SERVER SIDE CODE
 Meteor.startup(function () {	
-	FlickrSetList(apiKey,userID,flickrDB,flickrDBKey,function(){
-		var setList = flickrDB.find({name:flickrDBKey});
-		setList.forEach(function(eachSetItem){
-			var flickrSetID=eachSetItem.data.id;
-			FlickrSetPhotos(apiKey,flickrSetID,flickrDBKey);
-		});
-		Meteor.publish("sets",function(){
-			return flickrDB.find(
-				{},
-				{fields: {id:1,data:1,photos:1}}
-			);
+	FlickrUserID(apiKey,userName,function(){
+		FlickrSetList(apiKey,userID,flickrDB,flickrDBKey,function(){
+			var setList = flickrDB.find({name:flickrDBKey});
+			setList.forEach(function(eachSetItem){
+				var flickrSetID=eachSetItem.data.id;
+				FlickrSetPhotos(apiKey,flickrSetID,flickrDBKey);
+			});
+			Meteor.publish("sets",function(){
+				return flickrDB.find(
+					{},
+					{fields: {id:1,data:1,photos:1}}
+				);
+			});
 		});
 	});
 });
