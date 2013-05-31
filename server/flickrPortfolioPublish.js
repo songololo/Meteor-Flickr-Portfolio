5a7a3ef54137476685ad8447a993fc46b60a8e82
shongololo <garethsimons@icloud.com>
2013-05-30 23:38:13 -0500

    Draft working version.

--- server/flickrPortfolioPublish.js
+++ server/flickrPortfolioPublish.js
@@ -6,7 +6,7 @@
 //// VARIABLES:
 var apiKey = "b4b033a1b3c8f74573e021bd37565336";
 var userName = "garethsimons";
-userID = null //"78352164@N07"; Global variable - set in flickr.js
+userID = null; //"78352164@N07"; Global variable - set in flickr.js
 var flickrDBKey = "flickrSets";
 
 
@@ -19,6 +19,7 @@ Meteor.startup(function () {
 			setList.forEach(function(eachSetItem){
 				var flickrSetID=eachSetItem.data.id;
 				FlickrSetPhotos(apiKey,flickrSetID,flickrDBKey);
+				flickrDB._ensureIndex({random:1});
 			});
 			Meteor.publish("sets",function(){
 				return flickrDB.find(
@@ -29,3 +30,23 @@ Meteor.startup(function () {
 		});
 	});
 });
+
+Meteor.methods({
+	randomURL: function(){
+		var setCount = flickrDB.find().count();
+		var setRandom = Math.floor(Math.random()*setCount);
+		var setObject = flickrDB.findOne(
+			{setNum:setRandom},
+			{fields:{"photos.total":1, "photos.photo":1}});
+		var photoCount = setObject.photos.total;
+		var photoRandom = Math.floor(Math.random()*photoCount);
+		// return only images wider than they are high
+		while (setObject.photos.photo[photoRandom].width_o <
+			setObject.photos.photo[photoRandom].height_o){
+				photoRandom = Math.floor(Math.random()*photoCount);
+			}
+		var url = setObject.photos.photo[photoRandom].url_o;
+		console.log("Random set index "+setRandom+" with random photo index "+photoRandom+" with a URL of "+url);
+		return url;
+		}
+	});
