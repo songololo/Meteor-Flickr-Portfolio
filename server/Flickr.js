281c527efc908e2b24d7cb49a2eb25dd9cb0507f
shongololo <garethsimons@icloud.com>
2013-06-04 22:18:55 -0500

    Incremental Update

--- server/Flickr.js
+++ server/Flickr.js
@@ -1,4 +1,4 @@
-//// FLICKR API LIBRARY
+//// FLICKR API LIBRARY ////
 // Note that you need the Meteor HTTP package for the calls
 
 // Retrieves userID
@@ -40,6 +40,7 @@ FlickrSetList = function(apiKey,userID,flickrDB,flickrDBKey,callback){
 
 // Retrieves set's photos                
 FlickrSetPhotos = function(apiKey,flickrSetID,flickrDBKey,callback){
+	console.log("Photos retrieved from "+flickrSetID);
 	Meteor.http.call("GET","http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key="+apiKey+"&photoset_id="+flickrSetID+"&extras=date_taken,tags,machine_tags,path_alias,url_sq,url_t,url_s,url_m,url_o&format=json&nojsoncallback=1", function(error,result){
 		if (result.statusCode === 200) {
 			var photos = JSON.parse(result.content);
@@ -52,21 +53,6 @@ FlickrSetPhotos = function(apiKey,flickrSetID,flickrDBKey,callback){
 		}
 		if (callback && typeof(callback) === "function") {  
 			callback();
-		}
-	});
-};
-
-// Calls random photo from particular set
-FlickrRandomPhotoFromSet = function(apiKey,setID){
-	Meteor.http.call("GET","http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key="+apiKey+"&photoset_id="+setID+"&format=json&nojsoncallback=1",function (error, result) {
-		if (result.statusCode === 200) {
-//console.log(result.content);
-			var photoResult = JSON.parse(result.content);
-			var photoCount = photoResult.photoset.total;
-			var randomPhoto = Math.floor((Math.random()*photoCount)+1);
-			var selectedPhoto = photoResult.photoset.photo[randomPhoto];
-			var imageURL = 'http://farm'+selectedPhoto.farm+'.staticflickr.com/'+selectedPhoto.server+'/'+selectedPhoto.id+'_'+selectedPhoto.secret+'_b.jpg';
-			Session.setDefault("RandomURL",imageURL);		
 		}
 	});
 };
